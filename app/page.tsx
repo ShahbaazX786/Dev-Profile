c98dd85c395732f3347c0158679385086240ee07
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-01-14 13:34:03 +0530

    Added User Details placeholder UI elements.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import ThemeSwitcher from "@/components/ThemeSwitcher"
 import Image from "next/image"
 import Link from "next/link"
 
+import { MdLocationOn } from "react-icons/md";
+import { FiLink } from "react-icons/fi";
+import { RiTwitterXLine } from "react-icons/ri";
+import { BsFillBuildingsFill } from "react-icons/bs";
+
+
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full p-2 sm:p-4 transition-all pt-10 sm:pt-12 bg-slate-100 dark:bg-slate-950">
@@ -24,6 +32,41 @@ export default function Home() {
                   <Link href={'#'} className="text-blue-500 hover:underline text-sm transition-all" >Link</Link>
                 </div>
                 <p>Joined Date</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem quam voluptas quis minima commodi officiis dolorum perferendis nam eos recusandae?</p>
+              <div className="flex justify-between gap-3 rounded-lg bg-stone-100 dark:bg-slate-900 px-6 py-4 min-h-[50px]:">
+                <div className="flex  flex-col items-center gap-2">
+                  <p className="text-xs opacity-60">Repos</p>
+                  <p className="text-sm font-bold sm:text-base">200</p>
+                </div>
+                <div className="flex  flex-col items-center gap-2">
+                  <p className="text-xs opacity-60">Followers</p>
+                  <p className="text-sm font-bold sm:text-base">200</p>
+                </div>
+                <div className="flex  flex-col items-center gap-2">
+                  <p className="text-xs opacity-60">Following</p>
+                  <p className="text-sm font-bold sm:text-base">200</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                <div className="flex items-center gap-2">
+                  <MdLocationOn className="text-xl" />
+                  <p>Kurnool</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <FiLink className="text-xl" />
+                  <p>githubusername</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <RiTwitterXLine className="text-xl" />
+                  <p>Twitter</p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <BsFillBuildingsFill className="text-xl" />
+                  <p>Organization</p>
+                </div>
               </div>
             </div>
           </main>
